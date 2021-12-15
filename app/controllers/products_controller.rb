0bd5eb1c1f658578081ba9f0f7ffdf91d5aeb4e0
Will Holmes <willholmes60@gmail.com>
2021-12-15 15:10:42 +0000

    built table and simple form input

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,11 +5,16 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new(user: current_user)
   end
 
   def create
-
+    product = Product.new(product_params)
+    if product.save
+      redirect_to products_path
+    else
+      redirect_to new_product_path
+    end
   end
 
   def edit
@@ -19,5 +24,11 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :size, :prices)
   end
 end
