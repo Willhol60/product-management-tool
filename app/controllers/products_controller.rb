f00a05ee9f1bc1ff2cc4a17c85afb6eaca5374c7
Will Holmes <willholmes60@gmail.com>
2021-12-17 10:52:12 +0000

    admin panel and initial styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,10 +10,11 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(product_params)
+    product.user_id = current_user.id
     if product.save
       redirect_to products_path
     else
-      redirect_to new_product_path
+      render 'products/new'
     end
   end
 
@@ -24,11 +25,13 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    product = Product.find(params[:id])
+    redirect_to products_path if product.destroy
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :size, :prices)
+    params.require(:product).permit(:name, :size, :price_ids)
   end
 end
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,7 @@
 class Price < ApplicationRecord
   belongs_to :product
+
+  def label_function
+    "#{currency}#{value}"
+  end
 end
